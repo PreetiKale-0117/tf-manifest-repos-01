b397ae302eaec40a6142b40ab8f50ddb8b5015af
Anil_Rahod <anilrathod.it@gmail.com>
2022-12-19 13:00:50 +0530

    Added new TF resoures scripts

---
+++ tf_resources.tf
@@ -0,0 +1,34 @@
+resource "aws_instance" "web_sev" {
+  ami = var.appser_imageid
+  instance_type = var.appser_instancetype
+  key_name = var.appser_keypair
+  tags = {
+    "Name" = "preeti_appsev"
+  }
+  vpc_security_group_ids = [ aws_security_group.allow_https_sg.id ]
+  }
+
+  resource "aws_security_group" "allow_https_sg" {
+  name        = "allow_https_sg"
+  description = "Allow https inbound traffic"
+  
+
+  ingress {
+    description      = "HTTPS VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "PR_ALLOW_HTTPS"
+  }
+}
